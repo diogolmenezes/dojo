83c5d7605ea9da75a3b1f0b082b0f59c5c1c992f
diogolmenezes <diogolmenezes@gmail.com>
2014-02-11 13:37:36 -0200

    ajuste nos nomes

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -30,40 +30,40 @@ class FizzBuzz
 end
 
 
-class BancoTest < Test::Unit::TestCase
+class FizzBuzzTest < Test::Unit::TestCase
     def setup
         @fizzBuzz = FizzBuzz.new
     end
 
-    def test_1_say_1
+    def test_1_fale_1
         assert_equal(1, @fizzBuzz.jogar(1))
     end
 
-    def test_2_say_2
+    def test_2_fale_2
         assert_equal(2, @fizzBuzz.jogar(2))
     end
 
-    def test_3_say_fizz
+    def test_3_fale_fizz
         assert_equal("Fizz", @fizzBuzz.jogar(3))
     end
 
-    def test_6_say_fizz
+    def test_6_fale_fizz
         assert_equal("Fizz", @fizzBuzz.jogar(6))
     end
 
-    def test_5_say_buzz
+    def test_5_fale_buzz
         assert_equal("Buzz", @fizzBuzz.jogar(5))
     end
 
-    def test_10_say_buzz
+    def test_10_fale_buzz
         assert_equal("Buzz", @fizzBuzz.jogar(10))
     end
 
-    def test_15_say_fizzbuzz
+    def test_15_fale_fizzbuzz
         assert_equal("FizzBuzz", @fizzBuzz.jogar(15))
     end
 
-    def test_30_say_fizzbuzz
+    def test_30_fale_fizzbuzz
         assert_equal("FizzBuzz", @fizzBuzz.jogar(30))
     end
 end
