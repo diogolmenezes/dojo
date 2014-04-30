68edb4b232f513e096f42bea07e17ff52b40cad5
unknown <dmenezes@mttdev44.krypton.metatron.com.br>
2014-04-30 15:02:36 -0300

    incluindo validacao de valor invalido no caixa php

--- caixa-eletronico.php
+++ caixa-eletronico.php
@@ -1,11 +1,20 @@
 <?php
 
-  //
-  // Configurar um Builder no Sublime
-  // {
-  //    "shell_cmd": "C:\\wamp\\bin\\php\\php5.4.12\\php.exe C:\\wamp\\bin\\php\\php5.4.12\\phpunit.phar --colors $file"
-  // }
-  //
+  # Desenvolva um programa que simule a entrega de notas quando um cliente efetuar um saque em um caixa eletrônico. Os requisitos são os seguintes:
+  # • Entregar o menor número de notas;
+  # • Verificar se é possível sacar o valor solicitado com as notas disponíveis;
+  # • Saldo do cliente infinito;
+  # • A quantidade de notas do caixa eletrônico é infinita;
+  # • Notas disponíveis de R$ 100,00; R$ 50,00; R$ 20,00 e R$ 10,00;
+  #
+  # Exemplos:
+  # • Valor do Saque: R$ 30,00 – Resultado Esperado: Entregar 1 nota de R$20,00 e 1 nota de R$ 10,00.
+  # • Valor do Saque: R$ 80,00 – Resultado Esperado: Entregar 1 nota de R$50,00 1 nota de R$ 20,00 e 1 nota de R$ 10,00.
+  #
+  # Para configurar um builder no Sublime:
+  # {
+  #    "shell_cmd": "C:\\wamp\\bin\\php\\php5.4.12\\php.exe C:\\wamp\\bin\\php\\php5.4.12\\phpunit.phar --colors $file"
+  # }
 
   class CaixaEletronico
   {
@@ -17,6 +26,11 @@
     public function Sacar($valor)
     {
       $notas = array();
+      $valorPossivel = ($valor % 10 == 0);
+
+      if(!$valorPossivel)
+        throw new Exception("Valor inválido.");
+
 
       foreach($this->notasDisponiveis as $nota)
       {
@@ -46,6 +60,24 @@
     public function test_sacar_0_entao_0_notas()
     {
         $this->assertEquals(array(), $this->CaixaEletronico->Sacar(0));
+    }
+
+    /**
+    * @expectedException Exception
+    * @expectedExceptionMessage Valor inválido.
+    */
+    public function test_sacar_2_entao_0_notas()
+    {
+      $this->CaixaEletronico->Sacar(2);
+    }
+
+    /**
+    * @expectedException Exception
+    * @expectedExceptionMessage Valor inválido.
+    */
+    public function test_sacar_53_entao_0_notas()
+    {
+      $this->CaixaEletronico->Sacar(53);
     }
 
     public function test_sacar_10_entao_retorna_1_nota_10()
